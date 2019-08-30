5f51f76376ca7379a7344c3fb07155607a6e8d65
niranjan-mj <niranjankumar.mj@vvdntech.in>
2019-08-30 10:06:02 +0530

    design final 1st page

---
+++ quizapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from quizapp import models
+
+admin.site.register(models.StudentDetails)
---
+++ quizapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class StudentDetails(models.Model):
+    dob = models.DateField()
+    email_id = models.EmailField(max_length=254, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    score = models.IntegerField(max_length=2)
